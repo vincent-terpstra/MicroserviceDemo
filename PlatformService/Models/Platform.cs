8fa5838683857e106193758c2619fedc602ad5b0
Vincent Terpstra <vdterpst@gmail.com>
2022-11-14 13:20:58 -0500

    Added models

---
+++ PlatformService/Models/Platform.cs
@@ -0,0 +1,12 @@
+﻿namespace PlatformService.Models;
+
+public class Platform
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Publisher { get; set; }
+
+    public string Cost { get; set; }
+}
