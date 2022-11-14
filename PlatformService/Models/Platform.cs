f1e3e260b2b7d18341c3856e2606317a68ed2488
Vincent Terpstra <vdterpst@gmail.com>
2022-11-14 13:22:41 -0500

    Add in Data Annotations

--- PlatformService/Models/Platform.cs
+++ PlatformService/Models/Platform.cs
@@ -1,12 +1,19 @@
-﻿namespace PlatformService.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PlatformService.Models;
 
 public class Platform
 {
+    [Key]
+    [Required]
     public int Id { get; set; }
 
+    [Required]
     public string Name { get; set; }
 
+    [Required]
     public string Publisher { get; set; }
 
+    [Required]
     public string Cost { get; set; }
 }
